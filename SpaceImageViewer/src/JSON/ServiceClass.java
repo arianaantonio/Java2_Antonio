cc5405d8bcf3d8cf0d847202c51d8d362788fd36
p32424 <faeryqueen21@fullsail.edu>
2014-07-08 22:52:32 -0700

    Added ServiceClass

---
+++ SpaceImageViewer/src/JSON/ServiceClass.java
@@ -0,0 +1,19 @@
+package JSON;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ServiceClass extends IntentService {
+
+	public ServiceClass(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
