049ca215d45c753b2f59f0bd9390388f9097fe9b
p32424 <faeryqueen21@fullsail.edu>
2014-07-10 02:40:37 -0700

    Created Handler in MainActivity

--- SpaceImageViewer/gen/com/arianaantonio/spaceimageviewer/R.java
+++ SpaceImageViewer/gen/com/arianaantonio/spaceimageviewer/R.java
@@ -29,10 +29,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int imageView1=0x7f090002;
-        public static final int listView1=0x7f090000;
-        public static final int my_image=0x7f090001;
+        public static final int action_settings=0x7f090004;
+        public static final int imageView1=0x7f090003;
+        public static final int listView1=0x7f090001;
+        public static final int my_image=0x7f090002;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -46,7 +47,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int detail_view=0x7f050003;
-        public static final int select_text=0x7f050002;
+        public static final int textview_text=0x7f050002;
     }
     public static final class style {
         /** 
