62d764bb0fce8f71de243ee1654186c9c21d1ff5
p32424 <faeryqueen21@fullsail.edu>
2014-07-10 19:25:06 -0700

    Created custom listview xml file

--- SpaceImageViewer/gen/com/arianaantonio/spaceimageviewer/R.java
+++ SpaceImageViewer/gen/com/arianaantonio/spaceimageviewer/R.java
@@ -29,15 +29,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int imageView1=0x7f090003;
+        public static final int action_settings=0x7f090007;
+        public static final int camera=0x7f090005;
+        public static final int imageView1=0x7f090006;
         public static final int listView1=0x7f090001;
         public static final int my_image=0x7f090002;
         public static final int textView1=0x7f090000;
+        public static final int title=0x7f090003;
+        public static final int user=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int image_list=0x7f030001;
+        public static final int advance_listview=0x7f030001;
+        public static final int image_list=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
